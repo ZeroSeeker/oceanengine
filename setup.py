5530fe3481e87200f136e2e9987af0d2b3854ffd
ZeroSeeker <zeroseeker@foxmail.com>
2022-03-14 18:51:46 +0800

    初始化

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# coding = utf8
+"""
+@ Author : ZeroSeeker
+@ e-mail : zeroseeker@foxmail.com
+@ GitHub : https://github.com/ZeroSeeker
+@ Gitee : https://gitee.com/ZeroSeeker
+"""
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oceanengine",
+    version="0.0.1",
+    author="ZeroSeeker",
+    author_email="zeroseeker@foxmail.com",
+    description="make it easy to use oceanengine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/ZeroSeeker/oceanengine",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests==2.27.1'
+    ]
+)
