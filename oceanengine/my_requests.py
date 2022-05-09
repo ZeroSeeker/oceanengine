93f72321e5f8f8c3c4c035f29387c9893113bb1e
ZeroSeeker <zeroseeker@foxmail.com>
2022-05-09 12:33:14 +0800

    增加requests_json方法

--- oceanengine/my_requests.py
+++ oceanengine/my_requests.py
@@ -11,7 +11,7 @@ import showlog
 import time
 
 
-def my_requests(
+def requests_timeout(
         method,
         url,
         timeout_retry: bool = True,
@@ -21,19 +21,58 @@ def my_requests(
     timeout_retry: 超时重试，为True会自动重试，否则不会自动重试
     """
     while True:
-        if timeout_retry is True:
-            try:
-                return requests.request(
-                    method=method,
-                    url=url,
-                    **kwargs
-                )
-            except requests.exceptions.ReadTimeout:
-                showlog.warning('连接超时，将在1秒后重试...')
-                time.sleep(1)
-        else:
+        try:
             return requests.request(
                 method=method,
                 url=url,
                 **kwargs
             )
+        except requests.exceptions.ReadTimeout:
+            if timeout_retry is True:
+                showlog.warning('连接超时，将在1秒后重试...')
+                time.sleep(1)
+            else:
+                return requests.request(
+                    method=method,
+                    url=url,
+                    **kwargs
+                )
+
+
+def requests_json(
+        method,
+        url,
+        timeout_retry: bool = True,
+        json_error_retry: bool = True,
+        **kwargs
+):
+    """
+    timeout_retry: 超时重试，为True会自动重试，否则不会自动重试
+    """
+    while True:
+        try:
+            return requests.request(
+                method=method,
+                url=url,
+                **kwargs
+            ).json()
+        except requests.exceptions.ReadTimeout:
+            if timeout_retry is True:
+                showlog.warning('连接超时，将在1秒后重试...')
+                time.sleep(1)
+            else:
+                return requests.request(
+                    method=method,
+                    url=url,
+                    **kwargs
+                ).json()
+        except requests.exceptions.JSONDecodeError:
+            if json_error_retry is True:
+                showlog.warning('返回json格式错误，将在1秒后重试...')
+                time.sleep(1)
+            else:
+                return requests.request(
+                    method=method,
+                    url=url,
+                    **kwargs
+                ).json()
